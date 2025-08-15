a6096da93925e82c275671b7148c22275a39ed7f
AdemirJPro <165808195+AdemirJPro@users.noreply.github.com>
2025-08-14 23:37:58 -0400

    Update RenomearLista.bat

--- RenomearLista.bat
+++ RenomearLista.bat
@@ -1,13 +1,18 @@
+:: Renomei em massa uma lista em um arquivo TXT.
+:: Está progrmado para pegar um nome, como "nome1" e transformar em "nome1.ini"
+
 @echo off
 setlocal EnableDelayedExpansion
 
 :: Defina o nome do arquivo
 set "leitor=lista.txt"
+set "novoNome=.ini"
 
 :: Loop para ler cada linha do arquivo
 for /f %%a in ('type "%leitor%"') do (
-    echo %%a.ini>> NovaLista.txt
+    echo %%a%novoNome%>> NovaLista.txt
 )
 pause
 :: Fim do script
+
 endlocal
