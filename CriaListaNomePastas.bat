c85c90d2dd64c15f59d78ba947b87790f405a8a4
AdemirJPro <165808195+AdemirJPro@users.noreply.github.com>
2025-08-15 17:36:05 -0400

    Update CriaListaNomePastas.bat

--- CriaListaNomePastas.bat
+++ CriaListaNomePastas.bat
@@ -1,3 +1,5 @@
+:: Cria uma lista com os nomes das pastas do local.
+
 @echo off
 setlocal
 
@@ -5,5 +7,6 @@ for /f "delims=" %%a in ('dir /b /ad "%~dp0\*.*"') do (
   echo %%a >> ListaNomesPastas.txt
   set %%a=+1
 )
+
 
 endlocal
