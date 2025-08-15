307b6caf131262d63c4ad740d66cc6600c73ab21
AdemirJPro <165808195+AdemirJPro@users.noreply.github.com>
2025-08-14 23:31:13 -0400

    Update Fecha SET.bat

--- Fecha SET.bat
+++ Fecha SET.bat
@@ -1,5 +1,9 @@
+:: Ao abrir o bat, digite o nome do executável que deseja finalizar e aperte Enter.
+:: Se mais de um processo estiver aberto, todos irão fechar.
+
 @echo off
 echo Digite o nome do arquivo executavel para ser fechado:
 set /p "arquivo=Arquivo: "
+
 
 taskkill /f /im "%arquivo%.exe"
