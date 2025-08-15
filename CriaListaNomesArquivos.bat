161d620c238bb46bdb7604d3e0c246c2bc4751ed
AdemirJPro <165808195+AdemirJPro@users.noreply.github.com>
2025-08-14 23:28:56 -0400

    Update CriaListaNomesArquivos.bat

--- CriaListaNomesArquivos.bat
+++ CriaListaNomesArquivos.bat
@@ -1,8 +1,11 @@
+:: Cria uma lista o nome de todos os arquivos onde o bat está.
+
 @echo off
 setlocal
 
 for /f "delims=" %%a in ('dir /b "%~dp0\*.*"') do (
   echo %%a >> ListaNomesArquivos.txt
 )
+
 
 endlocal
